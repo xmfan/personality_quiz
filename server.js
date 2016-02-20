3694bfbc2bfed00d7cedacf32820a5a98acb6b20
Jefftree <jeffrey.ying86@live.com>
2016-02-20 14:51:51 -0500

    frontend template

--- server.js
+++ server.js
@@ -2,7 +2,13 @@
 
 var express = require('express'),
 	app = express();
+var bodyParser = require('body-parser');
 
-app.listen(4000, function() {
+var port = process.env.PORT || 4000;
+
+app.use(bodyParser.json());
+app.use(express.static('public'));
+
+app.listen(port, function() {
 	console.log('Server running on: 4000');
 });
